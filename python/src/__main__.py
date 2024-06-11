61402e568d36cfa6b54e8368323747c0d47f2c9b
stove-sakaya-teruyoshi <pc-107@st-ove.co.jp>
2024-06-11 10:39:21 +0900

    仮想環境の挙動がおかしかったのを直し、動くようにした #5

--- python/src/__main__.py
+++ python/src/__main__.py
@@ -2,44 +2,50 @@ import requests
 import re
 
 def main():
-    my_reg()
+    url = "https://coconala.com/categories/9"
+    content = my_fetch(url)
 
-def my_reg():
-    with open('coconala-scraping.txt', 'r', encoding='utf-8') as file:
-        content = file.read()
-        items = get_item(content)
-        
-        result_str = ""
-        count = 0
+    # content = ""
+    # with open('coconala-scraping.txt', 'r', encoding='utf-8') as file:
+    #     content = file.read()
+    # if content != None:
+    #     return
+    
+    my_reg(content)
 
-        for item in items:
-            title = get_title(item)
-            price = get_price(item)
-            item_status = title + " : " + price
-            if count < 10:
-                result_str += item_status + "\n"
-            count += 1
-            
-        with open("coconala-scraping-div.txt", "w") as f:
-            f.write(result_str)
+def my_reg(content):
+    items = get_items(content)
+    
+    result_str = ""
+    count = 0
 
-def get_item(content):
-    pattern_item = "(<div class=\"c-searchPageItemBlock\".+?)<div class=\"c-searchPageItemBlock\""
-    item = re.findall(pattern_item, content, re.MULTILINE | re.DOTALL)
-    return item
+    for item in items:
+        title = get_title(item)
+        price = get_price(item)
+        item_status = title + " : " + price
+        if count < 10:
+            result_str += item_status + "\n"
+        count += 1
+
+    with open("coconala-scraping-div.txt", "w") as f:
+        f.write(result_str)
+
+def get_items(content):
+    pattern_item = r"(<div class=\"c-searchPageItemBlock\".+?)<div class=\"c-searchPageItemBlock\""
+    items = re.findall(pattern_item, content, re.MULTILINE | re.DOTALL)
+    return items
 
 def get_title(content):
-    pattern_title = "<h3 class=\"c-serviceBlockItemContent_name\"[^>]*>([^<]+)<"
+    pattern_title = r"<h3 class=\"c-serviceBlockItemContent_name\"[^>]*>([^<]+)<"
     title = re.findall(pattern_title, content, re.MULTILINE | re.DOTALL)
     return title[0]
 
 def get_price(content):
-    pattern_price = "<strong[^>]*>([\d|,]+)</strong>\s*(円)"
+    pattern_price = r"<strong[^>]*>([\d|,]+)</strong>\s*(円)"
     price = re.findall(pattern_price, content, re.MULTILINE | re.DOTALL)
     return price[0][0] + price[0][1]
 
-def my_fetch():
-    url = "https://coconala.com/categories/9"
+def my_fetch(url):
     user_agent = "SampleScraping/0.1.0"
     headers = {
         "User-Agent": user_agent
@@ -53,8 +59,10 @@ def my_fetch():
         textRes = response.text
         with open("coconala-scraping.txt", "w") as f:
             f.write(textRes)
+        return textRes
     else:
         print(f"Failed to fetch data. Status code: {response.status_code}")
+        return None
 
 if __name__ == "__main__":
     main()
